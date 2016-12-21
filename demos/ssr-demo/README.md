b2f80f8d9f491d049f0b155d043d0be29278ca44
Peter Johason <peter@peterjohanson.com>
2016-12-20 22:09:24 -0500

    Initial work on a (non-working) SSR demo.

---
+++ demos/ssr-demo/README.md
@@ -0,0 +1,26 @@
+# ssr-demo
+
+> A vue-mdc SSR Demo
+
+## Build Setup
+
+```
+# install dependencies
+npm install
+
+# [development]
+# start application in development (it's a good practice to use `nodemon`
+# in development which will automatically reload the server on code changes
+# e.g. `nodemon --exec npm start`)
+npm start
+
+# [production]
+# build production bundles, switch environment to production and run the server
+npm run build
+npm config set ssr-demo:env production
+npm start
+```
+
+## Tutorials
+
+Vue.js tutorials: [Vue.js Cheatsheet](https://xpepermint.gitbooks.io/vue-js-cheatsheet/content/)
---
+++ demos/ssr-demo/src/app/components/app.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="../assets/images/logo.png"/>
+    <p>Check <a href="https://github.com/xpepermint/vue-example/">vue-example</a> for more.</p>
+    <checkbox></checkbox>
+  </div>
+</template>
+
+<style lang="scss">
+$mdc-theme-primary: #f44336;
+$mdc-theme-accent: #f06292;
+
+@import '@material/typography/mdc-typography';
+@import '@material/ripple/mdc-ripple';
+@import '@material/elevation/mdc-elevation';
+@import '@material/list/mdc-list';
+@import '@material/theme/mdc-theme';
+
+@import '../assets/styles/scaffold.css';
+</style>
+
+<script>
+
+import Checkbox from '@v-material/checkbox/Checkbox';
+
+export default {
+  components: { Checkbox }
+};
+</script>
